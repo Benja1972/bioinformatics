30564609744ff83fb7f5a830bdac0896bb58b9bc
Benja1972 <benja@bk.ru>
2019-03-20 10:21:58 +0100

    update

--- Enh_Mut_Manip.py
+++ Enh_Mut_Manip.py
@@ -178,3 +178,148 @@ def bigWig2bed_pot(bw,bed,genome,pad=1e5,alpha=10, step=1):
     
     return df
 
+
+def deepbind(fa,md):
+    """
+    Parametes
+    ---------
+    md : ID of models
+    fs : sequence, e.g. 'AAGTAAGCTGAACC'
+    """
+    cmd = 'echo {} | deepbind --no-head {}'.format(fa,md)
+    res = os.popen(cmd).readlines()
+    score = float(res[0].replace('\n',''))
+    
+    return score
+
+
+
+def deepbind_list(md_l,fs):
+    """
+    Parametes
+    ---------
+    md_l : list of IDs of models
+    fs : sequence, e.g. 'AAGTAAGCTGAACC'
+    """
+    df_md = pd.DataFrame(md_l, columns=['model'])
+    df_md['score'] = df_md.apply(lambda row: deepbind(fs,row['model']), axis=1)
+    
+    return df_md 
+
+
+
+
+def snp(seq,pos,nt):
+    """
+    Blind SNP
+    """
+    seq = MutableSeq(seq)
+    seq[pos]=nt
+    return str(seq)
+
+
+def mut(fa,i,ref,alt):
+    """
+    General type mutation
+    """
+    seq = MutableSeq(fa)
+    ref = MutableSeq(ref)
+    alt =  MutableSeq(alt)
+    
+    ref_s = seq[i:i+len(ref)]
+    if (ref==ref_s):
+        seq_mut = seq[:i]+alt+seq[i+len(ref):]
+    else:
+        seq_mut = MutableSeq('')
+        print('It is not correct variant')
+        
+    return str(seq_mut)
+
+def mut_map(fa,md):
+    p0 = deepbind(fa,md)
+
+    df = pd.DataFrame(index=['A','C','G','T'],columns=list(fa))
+    for i in range(df.shape[0]):
+        for j in range(df.shape[1]):
+            nt = df.index[i]
+            p1 = deepbind(snp(fa,j,nt),md)
+            df.iloc[i,j] = (p1 - p0)*max([0.,p0,p1])
+            
+    df = df.astype(float)
+    
+    return df
+
+class Scale(matplotlib.patheffects.RendererBase):
+    def __init__(self, sx, sy=None):
+        self._sx = sx
+        self._sy = sy
+
+    def draw_path(self, renderer, gc, tpath, affine, rgbFace):
+        affine = affine.identity().scale(self._sx, self._sy)+affine
+        renderer.draw_path(gc, tpath, affine, rgbFace)
+
+def draw_motif(sm,ax):
+
+    colors = {'G': 'orange', 'A': 'darkgreen', 'C': 'blue', 'T': 'red'}
+
+    ax.set_xlim(0,len(sm))
+    ax.set_ylim(0,max(sm))
+
+    for i in range(sm.shape[0]):
+        nt = sm.index[i]
+        txt = ax.text(i,0, nt, fontsize=18, weight='bold', color=colors[nt])
+        txt.set_path_effects([Scale(1, sm[i])])
+    return ax
+
+
+def draw_logo(all_scores, fontfamily='Arial', size=80):
+
+    mpl.rcParams['font.family'] = fontfamily
+    
+    colors = {'G': 'orange', 'A': 'darkgreen', 'C': 'blue', 'T': 'red'}
+
+    fig, ax = plt.subplots(figsize=(len(all_scores), 2.5))
+
+    font = FontProperties()
+    font.set_size(size)
+    font.set_weight('bold')
+
+    ax.set_xticks(range(1,len(all_scores)+1))    
+    ax.set_yticks(range(0,3))
+    ax.set_xticklabels(range(1,len(all_scores)+1), rotation=90)
+    ax.set_yticklabels(np.arange(0,3,1))    
+    sns.despine(ax=ax, trim=True)
+    
+    trans_offset = transforms.offset_copy(ax.transData, 
+                                          fig=fig, 
+                                          x=1, 
+                                          y=0, 
+                                          units='dots')
+   
+    for index, scores in enumerate(all_scores):
+        yshift = 0
+        for base, score in scores:
+            txt = ax.text(index+1, 
+                          0, 
+                          base, 
+                          transform=trans_offset,
+                          #fontsize=80, 
+                          color=colors[base],
+                          ha='center',
+                          fontproperties=font,
+
+                         )
+            txt.set_path_effects([Scale(1.0, score)])
+            fig.canvas.draw()
+            window_ext = txt.get_window_extent(txt._renderer)
+            yshift = window_ext.height*score
+            trans_offset = transforms.offset_copy(txt._transform, 
+                                                  fig=fig,
+                                                  y=yshift,
+                                                  units='points')
+        trans_offset = transforms.offset_copy(ax.transData, 
+                                              fig=fig, 
+                                              x=1, 
+                                              y=0, 
+                                              units='points')
+    return fig
